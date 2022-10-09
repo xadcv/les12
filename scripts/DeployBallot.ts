5c27dbfafb1337b8d2578929edf75e865e9ef519
cambyses21 <adcv@hey.com>
2022-10-09 19:19:30 +0200

    Send a token to each group member

--- scripts/DeployBallot.ts
+++ scripts/DeployBallot.ts
@@ -1,14 +1,9 @@
 import { ethers } from "hardhat";
 import * as dotenv from "dotenv";
 import { TokenizedBallot, MyToken } from "../typechain-types";
+
 dotenv.config();
 
-const VOTERS = [
-    "0x1BfE27367178FcFa8BE2784387F10FCBFf0D6456",
-    "0x6EaCc549C6378CA0506452899a1210060Ee71C20",
-    "0x963625ac45cC696Ad5Fe467Ebf4d62C9542b7726",
-    "0xA89a06aDe3B2Cb55971D9240f11E39bc615b7049",
-];
 
 
 const PROPOSALS = ["Group 1", "Group 6", "Group 3"];
